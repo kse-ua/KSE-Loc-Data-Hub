e5a033f1884f85066fb56dca6f7cf5630bbb7990
ValHat <61630702+velgaks@users.noreply.github.com>
2022-07-31 13:59:21 +0500

    Update 01-composition-codes.R
    Valentyn solution

--- analysis/tasks/01-composition-codes.R
+++ analysis/tasks/01-composition-codes.R
@@ -10,6 +10,23 @@ ds_in <-
 # where `id` identifies hromadas (ATC) and `codes_*` contain the list of
 # radas that comprise the hromada at that time (`date_*`)
 
+ds_1 <- ds_in %>%
+  dplyr::select(id, codes_1, date_1) %>%
+  tidyr::separate(codes_1, into = c('code_1', 'code_2'), sep = ',', extra = 'merge') %>%
+  # problem with separate() - need to know number of columns in advance
+  tidyr::pivot_longer(!c(id, date_1), values_to = 'code', values_drop_na = TRUE) %>%
+  select(!name) %>%
+  rename(date = date_1)
+
+ds_2 <- ds_in %>%
+  dplyr::select(id, codes_2, date_2) %>%
+  tidyr::separate(codes_2, into = c('code_1', 'code_2', 'code_3'), sep = ',', extra = 'merge') %>%
+  tidyr::pivot_longer(!c(id, date_2), values_to = 'code', values_drop_na = TRUE) %>%
+  select(!name) %>%
+  rename(date = date_2)
+
+ds_out <- rbind(ds_1, ds_2) %>% arrange(id, date)
+
 # please develop a solution that would transform the data into the following form:
 
 ds_out <- tibble::tribble(
