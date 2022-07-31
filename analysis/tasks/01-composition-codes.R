38fc9d04397eac3811dc15e9d161f91cec5a57db
ValHat <61630702+velgaks@users.noreply.github.com>
2022-07-31 16:29:49 +0500

    Update 01-composition-codes.R

--- analysis/tasks/01-composition-codes.R
+++ analysis/tasks/01-composition-codes.R
@@ -10,22 +10,35 @@ ds_in <-
 # where `id` identifies hromadas (ATC) and `codes_*` contain the list of
 # radas that comprise the hromada at that time (`date_*`)
 
+
+### Valentyn solution
+# there will be a lot of repeating code chunks that should be vectorized or wrapped in a function
+
+# put all code_* and date_* into vectors to vectorize code in the future
+# codes <- names(dplyr::select(ds_in,contains("codes_")))
+# dates <- names(dplyr::select(ds_in,contains("date_")))
+
+# count how many different codes in a codes column
+ncol <- max(stringr::str_count(ds_in$codes_1, ","))+1
+ncol_1 <- max(stringr::str_count(ds_in$codes_2, ","))+1
+
+# split dataset and transform each one separately
+
 ds_1 <- ds_in %>%
   dplyr::select(id, codes_1, date_1) %>%
-  tidyr::separate(codes_1, into = c('code_1', 'code_2'), sep = ',', extra = 'merge') %>%
-  # problem with separate() - need to know number of columns in advance
+  tidyr::separate(codes_1, into = c(paste0('code_', c(1:ncol))), sep = ',') %>%
   tidyr::pivot_longer(!c(id, date_1), values_to = 'code', values_drop_na = TRUE) %>%
-  select(!name) %>%
-  rename(date = date_1)
+  dplyr::select(!name) %>%
+  dplyr::rename(date = date_1)
 
 ds_2 <- ds_in %>%
   dplyr::select(id, codes_2, date_2) %>%
-  tidyr::separate(codes_2, into = c('code_1', 'code_2', 'code_3'), sep = ',', extra = 'merge') %>%
+  tidyr::separate(codes_2, into = c(paste0('code_', c(1:ncol_1))), sep = ',') %>%
   tidyr::pivot_longer(!c(id, date_2), values_to = 'code', values_drop_na = TRUE) %>%
-  select(!name) %>%
-  rename(date = date_2)
+  dplyr::select(!name) %>%
+  dplyr::rename(date = date_2)
 
-ds_out <- rbind(ds_1, ds_2) %>% arrange(id, date)
+ds_out <- rbind(ds_1, ds_2) %>% dplyr::arrange(id, date)
 
 # please develop a solution that would transform the data into the following form:
 
