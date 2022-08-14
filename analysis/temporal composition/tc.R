0084350e6114aa22accbb6e7c52a9c985b5ec77d
ValHat <61630702+velgaks@users.noreply.github.com>
2022-08-14 15:20:42 +0500

    #3
    #3 prepared data for plotting, plotted number of amalgamed radas by year

--- analysis/temporal composition/tc.R
+++ analysis/temporal composition/tc.R
@@ -4,7 +4,7 @@
 #' date: "Last updated: `r Sys.Date()`"
 #' ---
 #+ echo=F ----------------------------------------------------------------------
-# rmarkdown::render(input = "./analysis/tasks/01-composition-codes.R") # run to knit, don't uncomment
+rmarkdown::render(input = "./analysis/temporal composition/tc.R") # run to knit, don't uncomment
 #+ echo=F ----------------------------------------------------------------------
 library(knitr)
 # align the root with the project working directory
@@ -17,54 +17,64 @@ cat("\014") # Clear the console
 #+ load-packages ---------------------------------------------------------------
 library(tidyverse)
 
+#+ declare-globals -------------------------------------------------------------
+path_rada    <- "./data-private/derived/rada_hromada.csv"
+path_hromada <- "./data-private/derived/hromada.csv"
+path_time   <- "./data-private/derived/time_rada.csv"    
+
+#+ declare-functions -----------------------------------------------------------
+
+
 #+ load-data -------------------------------------------------------------------
-source("C:/Users/Dell/Documents/GitHub/ua-de-center/manipulation/0-import.R")
+ds0_rada <- readr::read_csv(path_rada)
+ds0_hromada <- readr::read_csv(path_hromada)
+ds0_time <- readr::read_csv(path_time)
 
 #+ inspect-data ----------------------------------------------------------------
-ds1_event %>% glimpse()
 
-ds1_event %>%
+ds0_rada %>%
   explore::describe_all()
 
+ds0_hromada %>%
+  explore::describe_all()
+
+ds0_time %>%
+  explore::describe_all()
 #+ transform-data ----------------------------------------------------------------
-ds0_ts <- 
-  ds1_event %>% 
-  mutate(
-    across(
-      starts_with('rada_codes')
-      , as.character
-    ),
-    across(
-      starts_with('decision_date')
-      , as.character
-    )
-    ) %>%
-  tidyr::pivot_longer(
-    cols = !starts_with("hromada_code")
-  ) %>% 
-  mutate(
-    wave  = str_extract(name, "\\d$" )
-    , name = str_extract(name, "(?<=\\_)(.*?)(?=\\_)")
-    ) %>% 
-  pivot_wider(
-    names_from = "name", values_from = "value"
-  ) %>% 
-  mutate(
-    code = str_split(codes, ',')
-  ) %>% 
-  unnest(cols = c("code")) %>% 
-  select(-codes, -wave) %>% 
-  arrange(hromada_code, date, code) %>%
-  mutate(
-    # hromada_code = hromada_code %>% as.character()
-    date = date %>% as.Date()
-    # ,code = code %>% as.integer()
-  ) %>%
-  filter(
-    !is.na(date)
+
+# separating date in separate columns
+ds1_time  <- 
+  ds0_time %>%
+  mutate(year = lubridate::year(date)
+         , month = lubridate::month(date)
+         , day = lubridate::day(date)
+         , .after = date
   )
 
-ds0_ts %>%
-  explore::describe_all()
+# making NA for years when no events (from 2015 to 2020), then filling with 
+# previous number of distinct radas; when no hromada before - 0 radas
 
-##
+ds1 <- 
+  ds1_time %>% 
+  group_by(hromada_code, year) %>%
+  summarise(rada_count = n_distinct(rada_code), .groups = 'drop') %>%
+  group_by(hromada_code) %>%
+  complete(year = c(2015:2020)) %>%
+  fill(rada_count, .direction = 'down') %>%
+  mutate(rada_count = replace_na(rada_count, 0))
+
+#+ plot-data ----------------------------------------------------------------
+
+# Plot for number of amalgamated radas by year
+ds1 %>%
+  group_by(year) %>%
+  summarise(n_radas = sum(rada_count)) %>%
+  ggplot(aes(x=year, y=n_radas)) +
+  geom_bar(stat='identity', position = 'identity') +
+  geom_text(aes(label=n_radas), vjust = -0.2) +
+  theme_classic()
+
+# Then we can count radas in hromada by adding facet_wrap by hromadas to previous plot
+# problem: a lot of hromadas, look for interactive plotting
+
+
