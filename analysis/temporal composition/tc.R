46ab8f575a33f60f517eda53dfb884274998c60e
ValHat <61630702+velgaks@users.noreply.github.com>
2022-08-15 00:37:43 +0500

    #3
    #3 problem - many hromadas, so need to make many separate plots

--- analysis/temporal composition/tc.R
+++ analysis/temporal composition/tc.R
@@ -4,9 +4,11 @@
 #' date: "Last updated: `r Sys.Date()`"
 #' ---
 #+ echo=F ----------------------------------------------------------------------
-rmarkdown::render(input = "./analysis/temporal composition/tc.R") # run to knit, don't uncomment
+# rmarkdown::render(input = "./analysis/temporal composition/tc.R") # run to knit, don't uncomment
 #+ echo=F ----------------------------------------------------------------------
 library(knitr)
+requireNamespace("trelliscopejs"   )# Interactive faceting
+
 # align the root with the project working directory
 opts_knit$set(root.dir='../../')  #Don't combine this call with any
 #+ echo=F ----------------------------------------------------------------------
@@ -20,26 +22,33 @@ library(tidyverse)
 #+ declare-globals -------------------------------------------------------------
 path_rada    <- "./data-private/derived/rada_hromada.csv"
 path_hromada <- "./data-private/derived/hromada.csv"
-path_time   <- "./data-private/derived/time_rada.csv"    
+path_time   <- "./data-private/derived/time_rada.csv"  
+path_admin    <- "./data-private/derived/ua-admin-map.rds"
+path_plots <- './analysis/temporal composition/plots'
 
+
+Sys.setlocale("LC_CTYPE", "russian")
 #+ declare-functions -----------------------------------------------------------
 
+plot_hromada <- function(x, path = getwd()) {
+  tmp <- ds2 %>%
+    filter(hromada_code == x)
+  p <- ggplot(tmp, aes(x=year, y=rada_count)) +
+    geom_bar(stat='identity', position = 'identity') +
+    geom_text(aes(label=rada_count), vjust = -0.2) +
+    theme_classic()
+  ggsave(p, file=paste0(x, ".png"), path = path)
+}
 
 #+ load-data -------------------------------------------------------------------
-ds0_rada <- readr::read_csv(path_rada)
-ds0_hromada <- readr::read_csv(path_hromada)
 ds0_time <- readr::read_csv(path_time)
+ds_admin <- readr::read_rds(path_admin)
 
 #+ inspect-data ----------------------------------------------------------------
 
-ds0_rada %>%
-  explore::describe_all()
-
-ds0_hromada %>%
-  explore::describe_all()
-
 ds0_time %>%
   explore::describe_all()
+
 #+ transform-data ----------------------------------------------------------------
 
 # separating date in separate columns
@@ -61,7 +70,24 @@ ds1 <-
   group_by(hromada_code) %>%
   complete(year = c(2015:2020)) %>%
   fill(rada_count, .direction = 'down') %>%
-  mutate(rada_count = replace_na(rada_count, 0))
+  mutate(rada_count = replace_na(rada_count, 0)) %>%
+  ungroup()
+
+hromada_names <- ds_admin %>%
+  select(hromada_code, hromada_name) %>%
+  distinct()
+
+ds2 <- 
+  ds1 %>%
+  left_join(
+    hromada_names
+    ,by = "hromada_code"
+  )
+
+ds2 %>%
+  explore::describe_all()
+# number of distinct codes not equal to number of unique hromada names -
+# probaply some names are the same (1470 codes, 1323 names)
 
 #+ plot-data ----------------------------------------------------------------
 
@@ -74,7 +100,13 @@ ds1 %>%
   geom_text(aes(label=n_radas), vjust = -0.2) +
   theme_classic()
 
-# Then we can count radas in hromada by adding facet_wrap by hromadas to previous plot
-# problem: a lot of hromadas, look for interactive plotting
+# sample of hromadas for plotting
+l <- sample(ds2$hromada_code, 10)
+# l <- unique(ds2$hromada_code)
+# gets an error
 
+# separate plots for each hromada 
+for (k in l) {
+  plot_hromada(k, path = path_plots)
+}
 
