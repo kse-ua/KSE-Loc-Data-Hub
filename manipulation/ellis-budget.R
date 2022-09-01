e6dba7014fa00a10d768c71054cc980d5963a2ac
andkov_ab <koval.andrey@gmail.com>
2022-09-01 15:18:07 -0600

    Update ellis-budget.R
    #18

--- manipulation/ellis-budget.R
+++ manipulation/ellis-budget.R
@@ -24,18 +24,18 @@ eval_chunks <- TRUE
 cache_chunks <- TRUE
 report_render_start_time <- Sys.time()
 options(width=100) # number of characters to display in the output (dflt = 80)
-Sys.setlocale("LC_CTYPE", "ukr")
+Sys.setlocale("LC_CTYPE", "russian")
 #+ load-sources ------------------------------------------------------------
 base::source("./scripts/common-functions.R") # project-level
 #+ load-packages -----------------------------------------------------------
 library(tidyverse)
 
 #+ declare-globals -------------------------------------------------------------
-# ---- declare-globals ---------------------------------------------------------
 # printed figures will go here:
 prints_folder <- paste0("./manipulation/ellis-budget-prints/")
 if (!fs::dir_exists(prints_folder)) { fs::dir_create(prints_folder) }
 
+path_1 <- "./data-private/raw/revenues-1.csv"
 
 #+ declare-functions -----------------------------------------------------------
 
@@ -44,10 +44,72 @@ cat("\n# 2.Data ")
 
 #+ load-data, eval=eval_chunks -------------------------------------------------
 
+paths_budget <-  list.files("./data-private/raw/",pattern = "revenues-\\d.csv$",full.names = T)
+# names_budget <-  paths_budget %>% str_replace("^.+revenues-(\\d{1}).csv","\\1")
+
+ls_import <- list()
+for(i in seq_along(paths_budget)){
+  
+  ls_import[[i]] <- 
+    readr::read_delim(
+      file = paths_budget[[i]]
+      ,delim = ";"
+      , escape_double = FALSE
+      , locale = locale(encoding = "Windows-1251",asciify = TRUE)
+      , trim_ws = TRUE
+  ) %>% 
+    janitor::clean_names() %>%
+     mutate_all(.funs = as.character)
+}
+lapply(ls_import, glimpse)
+ds0 <- bind_rows(ls_import,.id = "file_number")
+ds0 %>% glimpse()
 #+ inspect-data ----------------------------------------------------------------
 
-#+ tweak-data, eval=eval_chunks ------------------------------------------------
+import_custom <- function(p){
+  d_out <- 
+   readr::read_delim(
+      file = p
+      ,delim = ";"
+      , escape_double = FALSE
+      , locale = locale(encoding = "Windows-1251",asciify = TRUE)
+      # , locale = locale(encoding = "Windows-1251",asciify = FALSE)
+      , trim_ws = TRUE
+    ) %>% 
+    janitor::clean_names() %>%
+    mutate_all(.funs = as.character)
+}
 
+d <- paths_budget[2] %>% import_custom()
+d %>% count(admin1)
+d %>% count(admin2)
+d %>% count(admin3)
+d %>% count(admin4) %>% arrange(admin4)
+
+d %>% filter(admin1 == "с-ща Озерянівка") %>% View()
+d %>% filter(admin2 == "10000000 Податкові надходження") %>% View()
+
+ds0 %>% count(as.factor(admin1))
+ds0 %>% count(as.factor(admin2)) %>% print_all()
+
+
+
+
+ds0 %>% 
+  filter(admin1 == "с-ща Іванівка") %>% 
+  View()
+d7 <- ls_import[[7]]
+d7 %>% count(admin1)
+ds0 %>% 
+  filter(admin2 == "10000000 Податкові надходження") %>% 
+  View()
+#+ tweak-data-1, eval=eval_chunks ------------------------------------------------
+ds0 %>% glimpse()
+ds1 <- 
+  ds0 %>% 
+  select(1:7)
+
+ds1 %>% glimpse()  
 #+ tweak-data-2 ----------------------------------------------------------------
 
 #+ tweak-data-3 ----------------------------------------------------------------
