da0d59eb7028b9a67d90a1298851b261016e34c7
Tytser <tytiuk.serhii@gmail.com>
2022-09-28 00:07:04 +0300

    stable ellis-geography (without polygons), added geography.csv

--- manipulation/ellis-geography.R
+++ manipulation/ellis-geography.R
@@ -32,15 +32,20 @@ library(tidyverse)
 library(tmap)
 library(tmaptools)
 library(osrm)
+library(sf)
 
 #+ declare-globals -------------------------------------------------------------
 path_file <- "./data-private/raw/ua-admin-codes.csv"
 
 path_oblast <- "./data-private/raw/oblast.csv"
 
-#Data on squares and number of settlements parsed from decenrtalization.org.ua
+#Data on areas and number of settlements parsed from decenrtalization.org.ua
 #Source: https://docs.google.com/spreadsheets/d/19Xxsq9O7fuHdNB4_GDyEMVSWBx3ENu3x9gNnnPrkqyo/edit?usp=sharing, sheet "parsed"
-path_squares <- "./data-private/raw/hromada_squares.csv"
+path_areas <- "./data-private/raw/hromada_squares.csv"
+
+#Shapefile of hromada polygons
+#Source: https://drive.google.com/file/d/1X3Ym-7s1RgQgsi_p4uJ3EGEWYGTd-UMz/view?usp=sharing
+path_polygons <-  "./data-private/raw/terhromad_fin.geojson"
 
 names_admin_ua <- c(
   "level_1"
@@ -52,7 +57,7 @@ names_admin_ua <- c(
   ,"object_name"
 )
 
-names_squares <- c(
+names_areas <- c(
   "oblast"
   ,"raion"
   ,"hromada_name"
@@ -71,13 +76,15 @@ cat("\n# 2.Data ")
 
 #+ load-data, eval=eval_chunks -------------------------------------------------
 ds0 <- readr::read_csv(path_file, col_names = names_admin_ua, skip=1)
-ds_hromada <- readr::read_csv("./data-public/derived/ua-pop-2022.csv")
+ds_hromada <- readr::read_csv("./data-private/derived/hromada.csv")
 ds0_oblast <- readr::read_csv(path_oblast, skip=0)
-ds0_squares <- readr::read_csv(path_squares, col_names = names_squares, skip=1)
+ds0_areas <- readr::read_csv(path_areas, col_names = names_areas, skip=1)
+ds_polygons <- st_read(path_polygons) %>% janitor::clean_names()
 
 #+ inspect-data ----------------------------------------------------------------
 
 #+ tweak-data, eval=eval_chunks ------------------------------------------------
+#selection of hromadas and hromada centers - they are arranged sequentially in the admin dataset
 data = tibble()
 i=0
 for (i in 1:length(ds0$level_1)) {
@@ -89,7 +96,12 @@ for (i in 1:length(ds0$level_1)) {
 ds1 <- 
   data %>% 
   mutate(
-    object_category = case_when(
+    hromada_type = case_when(
+      object_category == "C" ~ "сільська"
+      ,object_category == "X" ~ "сільська"
+      ,object_category == "M" ~ "міська"
+      ,object_category == "T" ~ "селищна")
+    ,object_category = case_when(
       object_category ==  "H" ~ "hromada"
       ,object_category %in% c("C", "M", "T", "X") ~ "hromada_center"
     )
@@ -103,19 +115,20 @@ ds1 <-
 ds2 <- 
   ds1 %>%
   left_join(
-    ds_hromada %>% select(hromada_code, oblast, raion, hromada_type)
+    ds_hromada %>% select(hromada_code, oblast_name, raion_name)
     ,by = c("level_3"="hromada_code")
   ) %>% 
   select(-c(level_1, level_2, level_extra)) %>%
   rename(hromada_code = level_3, hromada_center_code = level_4) %>% 
-  filter(is.na(oblast) == F) %>%
+  filter(is.na(oblast_name) == F) %>%
   mutate(
-    key1=paste(oblast,raion,hromada,hromada_center)
-    ,key2=paste(oblast,hromada_center)
+    key1=paste(oblast_name,raion_name,hromada,hromada_center)
+    ,key2=paste(oblast_name,hromada_center)
   )
 
-ds_squares <- 
-  ds0_squares %>% 
+#+ add-areas, eval=eval_chunks ------------------------------------------------
+ds_areas <- 
+  ds0_areas %>% 
   mutate(
     hromada_name = str_extract(hromada_name, ".+(?=\\sтериторіальна громада)")
     ,oblast = str_extract(oblast, "(.+(?=\\sобласть))|(м\\.\\sКиїв)")
@@ -125,26 +138,25 @@ ds_squares <-
 
 
 #+ add-geocodes, eval=eval_chunks ------------------------------------------------
+#add geocodes through request to API using combination of oblast_name,
+# raion_name,hromada,hromada_center
 ds2_geo1 <- 
   ds2 %>% 
   pull(key1) %>% 
   geocode_OSM(keep.unfound = T)
 
+# combination of oblast_name and hromada_center for hromada centers with invalid geocodes
 ds2_geo2<- 
   ds2 %>% 
   pull(key2) %>% 
   geocode_OSM(keep.unfound = T)
 
+# geocodes for oblast centers
 ds_oblast_geo <- 
   ds0_oblast %>% 
   mutate(key = paste(oblast_name, oblast_center)) %>% 
   pull(key) %>% 
   geocode_OSM(keep.unfound = T) 
-
-
-#+ add-squares, eval=eval_chunks ------------------------------------------------
-
-
 
 
 #+ combine ---------------------------------------------------------------------
@@ -173,9 +185,10 @@ ds_geo <-
       ,is.na(lon1) == T ~ lon2
     )
   ) %>% 
-  select(-c(lat1, lon1, lat2, lon2, key1, key2))
+  select(-c(lat1, lon1, lat2, lon2, key1, key2)) %>% 
+  filter(oblast_name != "Автономна Республіка Крим")
 
-
+#calculation of travel times between oblast centers and hromada centers
 times <- tibble()
 for (i in 1:length(ds_oblast$oblast_name)) {
   obl <- ds_oblast$oblast_name[i]
@@ -183,33 +196,35 @@ for (i in 1:length(ds_oblast$oblast_name)) {
     filter(oblast_name == obl) %>% 
     select(oblast_name, oblast_center_lat, oblast_center_lon)
   b <- ds_geo %>% 
-    filter(oblast == obl) %>% 
+    filter(oblast_name == obl) %>% 
     select(hromada_code, lat_center, lon_center)
   times <- rbind(times, 
     as_tibble(
-      osrmTable(src = b, dst = a)$durations
+      osrmTable(src = b, dst = a, measure = 'duration')$durations
       , rownames = "hromada_code") %>% 
       rename(ttime = ds_oblast$oblast_name[i])
   )
 }
 
+
+#combine all geo info in one dataset
 ds_geo_full <- 
   ds_geo %>% 
   left_join(
     times
     , by = "hromada_code") %>% 
-  mutate(key = paste(oblast,raion,hromada,hromada_type)) %>% 
+  mutate(key = paste(oblast_name,raion_name,hromada,hromada_type)) %>% 
   left_join(
-    ds_squares %>% select(key, n_settlements, square)
+    ds_areas %>% select(key, n_settlements, square)
     ,by = "key"
   ) 
+  # left_join(
+  #   ds_polygons %>% select(cod_3,geometry)
+  #   ,by = c("hromada_code"="cod_3")
+  # )
 
-
-
-#+ graph-1 ---------------------------------------------------------------------
-#+ graph-2 ---------------------------------------------------------------------
 #+ save-to-disk, eval=eval_chunks-----------------------------------------------
-readr::write_csv(ds_pop_22, "./data-public/derived/ua-pop-2022.csv")
+readr::write_csv(ds_geo_full, "./data-public/derived/geography.csv")
 
 
 
