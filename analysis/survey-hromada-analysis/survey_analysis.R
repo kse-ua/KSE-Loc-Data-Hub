c86724758c02879cbe4ab29c54fae000749e3faf
ValHat <61630702+velgaks@users.noreply.github.com>
2022-12-07 02:00:15 +0500

    Update survey_analysis.R

--- analysis/survey-hromada-analysis/survey_analysis.R
+++ analysis/survey-hromada-analysis/survey_analysis.R
@@ -20,8 +20,7 @@ library(fastDummies)
 library(lubridate)
 #+ declare-globals -------------------------------------------------------------
 # printed figures will go here:
-prints_folder <- paste0("./analysis/survey hromada analysis/prints
-                        ")
+prints_folder <- paste0("./analysis/survey-hromada-analysis/prints")
 if (!fs::dir_exists(prints_folder)) { fs::dir_create(prints_folder) }
 
 #+ declare-functions -----------------------------------------------------------
@@ -48,45 +47,121 @@ mcq<-survey_xls%>%
   pull()
 
 #vectors of mcq names
-preparation <- ds_survey %>% select(starts_with("prep_")) %>% colnames()
+preparation <- ds_survey %>% select(starts_with("prep_"), -prep_winter_count, -prep_count) %>% 
+  colnames()
 comm_channels <- ds_survey %>% select(telegram:hotline) %>% colnames()
-idp_help <- ds_survey %>% select(starts_with('idp_help/')) %>% colnames()
+idp_help <- ds_survey %>% select(starts_with('idp_help/'), -ends_with('number')) %>% colnames()
 military_help <- ds_survey %>% select(starts_with('help_for_military/')) %>% colnames()
 # only for occupied hromadas - few cases
-hromada_cooperation <- d1 %>% select(starts_with('hromada_cooperation/')) %>% colnames()
+hromada_cooperation <- ds_survey %>% select(starts_with('hromada_cooperation/')) %>% colnames()
 prep_for_winter <- c('info_campaign', 'reserves', 'count_power_sources', 
                      'count_heaters_need', 'solid_fuel_boiler')
 
+#+ recode variables ------------------------------------------------------------
+
+ds_survey <- ds_survey %>% 
+  mutate(income_own_per_capita = income_own_2021 / total_population_2022,
+         income_total_per_capita = income_total_2021 / total_population_2022,
+         income_tranfert_per_capita = income_transfert_2021 / total_population_2022)
+
 #+ ANALYSIS
 
-# correlations
-
-#SPEARMEN RANK CORRELATION FOR Q on preparations (between 14 items + total score + financial metrics)
-# ?financial metrics which represent preparation - income per capita, level of own income
+#+ correlations of preparation items with preparation index --------------------
 
 cor_mat <- 
-  cor(d3 %>% select(all_of(preparation), prep_count)
+  cor(ds_survey %>% select(all_of(preparation), prep_count)
       ,use = "complete.obs"
       ,method = "spearman")
 
-png(height=1800, width=1800, file="./analysis/prints/cor_preparation.png", type = "cairo")
+png(height=1800, width=1800, 
+    file="./analysis/survey-hromada-analysis/prints/cor_preparation.png", 
+    type = "cairo")
 
-corrplot::corrplot(cor_mat, tl.col = "black",tl.cex = 1.5, addCoef.col = "black", number.cex=1.5, order = "FPC")
+corrplot::corrplot(cor_mat, tl.col = "black",tl.cex = 1.5, 
+                   addCoef.col = "black", number.cex=1.5, order = "FPC")
 
 dev.off()
 
+#+ correlations of preparation index with economic indicators ------------------
+
+income <- ds_survey %>%
+  select(starts_with('income')) %>% colnames()
+
+cor_mat <- 
+  cor(ds_survey %>% select(all_of(income), prep_count, prep_winter_count)
+      ,use = "complete.obs"
+      ,method = "spearman")
+
+png(height=1800, width=1800, 
+    file="./analysis/survey-hromada-analysis/prints/cor_preparation_economics.png", 
+    type = "cairo")
+
+corrplot::corrplot(cor_mat[1:16,15:16, drop=F], tl.col = "black",tl.cex = 1.5, 
+                   addCoef.col = "black", number.cex=1.5, cl.pos = 'n')
+
+dev.off()
+
+#+ vol/nonvol amalgamation and preparation index ---------------------------------
+
+ds_survey %>%
+  group_by(voluntary) %>%
+  summarize(mean_prep = mean(prep_count)) %>%
+  ggplot(aes(x = factor(voluntary), y = mean_prep)) +
+  geom_text(aes(label = mean_prep)) +
+  geom_col()
+
+ds_survey %>%
+  group_by(voluntary) %>%
+  rstatix::get_summary_stats(prep_count, type = "mean_sd")
+
+res.aov <- ds_survey %>% rstatix::anova_test(prep_count ~ voluntary)
+# Result: doesn't differ
+
 #+ --------model for preparation and income before war -------------------------
-d3 %>%
-  # filter(income_own_per_capita < 15000) %>% 
-  ggplot(aes(income_own_per_capita, prep_count)) +
-  geom_point(aes(color = region_en)) +
+
+ds_survey %>%
+  # filter(income_total_per_capita < 15000) %>%
+  ggplot(aes(income_total_per_capita, prep_count)) +
+  geom_point(aes(color = type)) +
   geom_smooth(se=T, method = "lm")
 
-lm(data = d3 
+lm(data = ds_survey 
+   # filter(income_own_per_capita < 15000)
+   ,prep_count ~ income_total_per_capita
+) %>% summary()
+
+lm(data = ds_survey 
    # filter(income_own_per_capita < 15000)
    ,prep_count ~ income_own_per_capita
 ) %>% summary()
 
-hist(d3$income_own_per_capita)
+lm(data = ds_survey 
+   %>%
+   filter(income_tranfert_per_capita < 3000)
+   ,prep_count ~ income_tranfert_per_capita
+) %>% summary()
+
+hist(ds_survey$income_tranfert_per_capita)
+
+#+ administrative adaptation (percent of working) ------------------------------
+
+hist(ds_survey$percent_working_march)
+hist(ds_survey$percent_working_now)
+
+ds_survey <- ds_survey %>%
+  mutate(percent_working_march_cut = cut(percent_working_march, breaks = c(0,80,100), include.lowest = F),
+         percent_working_now_cut = cut(percent_working_now, breaks = c(0,80,100), include.lowest = F))
+
+plot(ds_survey$percent_working_march_cut)
+plot(ds_survey$percent_working_now_cut)
+
+ds_survey %>%
+  group_by(percent_working_march_cut) %>%
+  summarise(mean_prep = mean(prep_count, na.rm = T),
+            sd_prep = sd(prep_count, na.rm = T)) %>%
+ggplot(aes(x=percent_working_march_cut, y = mean_prep)) +
+  geom_col() +
+  geom_errorbar(aes(ymin=mean_prep-sd_prep, ymax=mean_prep+sd_prep), width=.2,
+                position=position_dodge(.9)) 
 
 #+ COMPARISON OF SURVEYED HROMADAS WITH GENERAL POPULATION OF HROMADAS ---------
