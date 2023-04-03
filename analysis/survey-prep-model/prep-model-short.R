b4ac6dd6ddabd4f741bda2903935706e2a6a8618
ValHat <valgat29@gmail.com>
2023-04-03 13:39:56 +0300

    Update prep-model-short.R

--- analysis/survey-prep-model/prep-model-short.R
+++ analysis/survey-prep-model/prep-model-short.R
@@ -45,97 +45,6 @@ data_cache_folder <- prints_folder # to sink modeling steps
 # ---- declare-functions -------------------------------------------------------
 '%ni%' <- Negate(`%in%`)
 
-rquery.cormat<-function(x,
-                        type=c('lower', 'upper', 'full', 'flatten'),
-                        graph=TRUE,
-                        graphType=c("correlogram", "heatmap"),
-                        col=NULL, ...)
-{
-  library(corrplot)
-  # Helper functions
-  #+++++++++++++++++
-  # Compute the matrix of correlation p-values
-  cor.pmat <- function(x, ...) {
-    mat <- as.matrix(x)
-    n <- ncol(mat)
-    p.mat<- matrix(NA, n, n)
-    diag(p.mat) <- 0
-    for (i in 1:(n - 1)) {
-      for (j in (i + 1):n) {
-        tmp <- cor.test(mat[, i], mat[, j], ...)
-        p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
-      }
-    }
-    colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
-    p.mat
-  }
-  # Get lower triangle of the matrix
-  getLower.tri<-function(mat){
-    upper<-mat
-    upper[upper.tri(mat)]<-""
-    mat<-as.data.frame(upper)
-    mat
-  }
-  # Get upper triangle of the matrix
-  getUpper.tri<-function(mat){
-    lt<-mat
-    lt[lower.tri(mat)]<-""
-    mat<-as.data.frame(lt)
-    mat
-  }
-  # Get flatten matrix
-  flattenCorrMatrix <- function(cormat, pmat) {
-    ut <- upper.tri(cormat)
-    data.frame(
-      row = rownames(cormat)[row(cormat)[ut]],
-      column = rownames(cormat)[col(cormat)[ut]],
-      cor  =(cormat)[ut],
-      p = pmat[ut]
-    )
-  }
-  # Define color
-  if (is.null(col)) {
-    col <- colorRampPalette(
-      c("#67001F", "#B2182B", "#D6604D", "#F4A582",
-                 "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE", 
-                 "#4393C3", "#2166AC", "#053061"))(200)
-                 col<-rev(col)
-  }
-  
-  # Correlation matrix
-  cormat<-signif(cor(x, use = "complete.obs", ...),2)
-  pmat<-signif(cor.pmat(x, ...),2)
-  # Reorder correlation matrix
-  ord<-corrMatOrder(cormat, order="hclust")
-  cormat<-cormat[ord, ord]
-  pmat<-pmat[ord, ord]
-  # Replace correlation coeff by symbols
-  sym<-symnum(cormat, abbr.colnames=FALSE)
-  # Correlogram
-  if(graph & graphType[1]=="correlogram"){
-    corrplot(cormat, type=ifelse(type[1]=="flatten", "lower", type[1]),
-             tl.col="black", tl.srt=45,col=col,...)
-  }
-  else if(graphType[1]=="heatmap")
-    heatmap(cormat, col=col, symm=TRUE)
-  # Get lower/upper triangle
-  if(type[1]=="lower"){
-    cormat<-getLower.tri(cormat)
-    pmat<-getLower.tri(pmat)
-  }
-  else if(type[1]=="upper"){
-    cormat<-getUpper.tri(cormat)
-    pmat<-getUpper.tri(pmat)
-    sym=t(sym)
-  }
-  else if(type[1]=="flatten"){
-    cormat<-flattenCorrMatrix(cormat, pmat)
-    pmat=NULL
-    sym=NULL
-  }
-  list(r=cormat, p=pmat, sym=sym)
-}
-
 # ---- load-data ---------------------------------------------------------------
 ds_survey <- readxl::read_excel("./data-private/derived/survey_hromadas_clean_new.xlsx")
 
@@ -344,12 +253,10 @@ cor_ds <- ds2 %>%
             urban_pct, passengers_2021_per_capita)) %>% 
   na.omit()
 
-cor_ds %>% 
-  summarise(across(everything(), ~sum(is.na(.)))) %>% 
-  t()
+cor <- cor(cor_ds, method = 'pearson')
+cor[cor == 1 | cor < 0.3 | is.na(cor)] <- ''
 
-cor(cor_ds, method = 'pearson') %>% neat_DT()
-
+g1 <- corrplot::corrplot(cor, method="circle", type = 'upper', tl.cex = .2)
 
 fit1_gaussian <- 
   glm(
