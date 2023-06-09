e01a0cfd014d86e198356669182a4e9252cea15c
ValHat <valgat29@gmail.com>
2023-06-09 13:34:33 +0300

    Update ellis-education-institutions.R

--- manipulation/ellis-education-institutions.R
+++ manipulation/ellis-education-institutions.R
@@ -83,8 +83,12 @@ ds1 <- ds1 %>%
          hromada_name = case_when(grepl(pattern, koatuu) ~ 'Київ', 
                                      .default = hromada_name),
          hromada_code = case_when(grepl(pattern, koatuu) ~ 'UA80000000000093317', 
-                                     .default = hromada_code))
+                                     .default = hromada_code),
+         match_status_koatuu = case_when(settlement_name == "Київ" ~ 'matched', 
+                                         .default = match_status_koatuu))
 
+matched_schools <- ds1 %>% filter(match_status_koatuu == "matched")
+  
 # dataset with unmatched koatuu
 unmatched_koatuu <- ds1 %>%  
   filter(match_status_koatuu == 'unmatched') %>% 
@@ -118,33 +122,102 @@ matched_schools_city <- city_schools %>% select(-c(24:31)) %>%
                hromada_code, raion_name, raion_code, oblast_name, oblast_code, key_code),
     by = c("key_code"))
 
+# check
+matched_schools_city %>% filter(is.na(hromada_name))
+
+
 # matching villages
 
-village_schools <- unmatched_schools %>% filter(grepl("с\\. |с-ще |смт ", settlement)) %>% 
+village_schools <- unmatched_schools %>% 
+  select(-c(settlement_name, settlement_code, hromada_name,
+         hromada_code, raion_name, raion_code, oblast_name, oblast_code)) %>% 
+  filter(grepl("с\\. |с-ще |смт ", settlement)) %>% 
   mutate(settlement_new = str_extract(settlement, "(?<=\\s)[^,]+"),
          oblast_new = str_extract(oblast, "^[^\\s]+"),
          raion_new = str_extract(settlement, "(?<=,\\s)([^,]+)(?=\\s)"),
+         raion_new = gsub("'", "’", raion_new),
          settlement_type = case_when(grepl("с\\.", settlement) ~ 'село',
                           grepl("смт", settlement) ~ 'селище міського типу',
                           grepl("с-ще", settlement) ~ 'селище'),
-         key_code = paste0(settlement_new, settlement_type, raion_new, oblast_new))
+         key_code = paste0(raion_new, oblast_new))
 
 ds_admin_villages <- ds_admin_map %>% filter(settlement_type != 'місто') %>% 
   filter(oblast_name_en != 'Crimea') %>% 
   mutate(key_code = paste0(settlement_name, settlement_type, raion_name, oblast_name))
 
+ds_admin_raion <- ds_admin_map %>% filter(oblast_name_en != 'Crimea') %>% 
+  mutate(key_code = paste0(raion_name, oblast_name)) %>% 
+  distinct(raion_name, raion_code, oblast_name, 
+           oblast_code, key_code)
 
-duplicates <- ds_admin_villages %>%
-  mutate(n = n(), .by = key_code) %>% 
-  filter(n>1) %>% 
-  arrange(key_code)
+village_schools %>% 
+  distinct(key_code) %>% 
+  filter(grepl("’", key_code)) %>% select(key_code)
 
+# Old raions in schools dataset!!!
+old_raions <- village_schools %>% 
+  anti_join(ds_admin_raion,
+            by = c("key_code")) %>% 
+  distinct(oblast_new, raion_new, key_code)
 
-matched_schools_villages <- village_schools %>% select(-c(24:31)) %>% 
+# reading file with old and new names comparison
+raions <- readxl::read_excel("raions.xlsx")
+
+d <- village_schools %>%
+  rename(raion_old = raion_new) %>%
+  left_join(raions %>% select(key_code, raion_new),
+            by = 'key_code') %>% 
+  mutate(raion_old = case_when(!is.na(raion_new) ~ raion_new,
+                               .default = raion_old),
+         raion_old = gsub("'", "’", raion_old),
+         key_code = paste0(raion_old, oblast_new))
+
+village_schools_raions <- d %>% 
   left_join(
-    ds_admin_villages %>% 
-      distinct(settlement_name, settlement_code, hromada_name,
-               hromada_code, raion_name, raion_code, oblast_name, oblast_code, key_code),
-    by = c("key_code"))
+    ds_admin_raion,
+    by = c("raion_old" = 'raion_name', 'oblast_new' = 'oblast_name')) %>% 
+  mutate(settlement_new = gsub("'", "’", settlement_new),
+         key_code = paste0(settlement_new, settlement_type, raion_old, oblast_new))
 
-write.csv(unmatched_schools, 'unmatched_schools.csv')
+ds_admin_villages_hromadas_unique <- ds_admin_villages %>% 
+  distinct(hromada_code, hromada_name, raion_name, raion_code, oblast_name, 
+           oblast_code, key_code) %>% 
+  mutate(n = n(), .by = c('key_code')) %>% 
+  filter(n == 1) %>% 
+  arrange(key_code) %>% 
+  select(-n)
+
+matched_schools_villages <- 
+  village_schools_raions %>%
+  rename(raion_name = raion_old, oblast_name = oblast_new) %>% 
+  left_join(
+    ds_admin_villages_hromadas_unique %>% select(hromada_name, hromada_code, key_code),
+    by = c("key_code")) %>% 
+  mutate(match_status_hromadas = ifelse(is.na(hromada_code), 'unmatched', 'matched'))
+  
+unmatched_schools_villages <- matched_schools_villages %>% 
+  filter(match_status_hromadas == 'unmatched') %>% 
+  select(-c(26:31))
+
+# merging it all together
+colnames(matched_schools)
+colnames(matched_schools_city)
+colnames(matched_schools_villages)
+
+d1 <- matched_schools %>% 
+  select(-c(settlement_code))
+  
+d2 <- matched_schools_city %>% 
+  select(-c(settlement_new, oblast_new, key_code, settlement_code))
+
+d3 <- matched_schools_villages %>% 
+  rename(settlement_name = settlement_new) %>% 
+  select(-c(key_code, match_status_hromadas, key_code.x, key_code.y, 
+            raion_new, settlement_type))
+
+fin <- rbind(d1, d2, d3)  
+
+# 2% unmatched - great!
+nrow(fin[is.na(fin$hromada_code),])/nrow(fin)
+
+write.csv(fin, './data-private/derived/matched_schools.csv')
