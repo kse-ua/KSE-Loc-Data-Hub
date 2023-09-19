543f9a22344c14d08d4943ccfaa9e570af673418
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2023-09-20 00:45:01 +0200

    Update ellis-tsnap.R
    90 NA hromada left

--- manipulation/ellis-tsnap.R
+++ manipulation/ellis-tsnap.R
@@ -59,43 +59,123 @@ ds0 %>% glimpse()
 
 metadata <- readxl::read_excel(path_tsnap, n_max = 1) %>% 
   t() 
-  
+
+# BIG DATA ORGANISING
+
+# Part 1 - extracting from address + getting rid of Kyiv
 ds1 <- ds0 %>% 
+  mutate(across(c(name, address), ~gsub("'", "’", .))) %>% # replacing for better processing
   mutate(
-    hromada_name = str_extract(gsub("[\\'’]", "’", gsub("\\([^()]*\\)", "", address)),
-                          "((\\p{L}+|\\p{L}+’\\p{L}+)(?=\\s+територіальна громада)|'(\\p{L}+\\s+центр послуг)\\s+м.\\p{L}+)")
-    ,settlement = str_extract(gsub("[\\'’]", "’", gsub("\\([^()]*\\)", "", address)), "(м\\.|с\\.|смт)\\s+(\\w+|м\\.[[:alpha:]]+)")
-    ,council = str_extract(name, "([а-яА-ЯіІїЇєЄґҐ’]+)\\s+(селищної ради|сільської ради|міської ради)")
-    ,oblast = str_extract(address, "[а-яА-ЯіІїЇєЄґҐ-]+(?= область)")
+    hromada_name = str_extract(address, "((\\p{L}+|[’'-]\\p{L}+)(?=\\s+територіальна громада)|'[’'](\\p{L}+\\s+центр послуг)\\s+м.\\p{L}+)")
+    ,settlement = str_extract(address, "(м\\.|с\\.|смт)\\s+([\\w’'-]+|м\\.[\\w’'-]+)")
+    ,council = str_extract(name, "([а-яА-ЯіІїЇєЄґҐ’'-]+)\\s+(селищної|сільської|міської)")
+    ,oblast_name = str_extract(address, "[а-яА-ЯіІїЇєЄґҐ’'-]+(?= область)")
   ) %>% 
+  mutate(oblast_name = case_when(
+    settlement == "м. Київ" ~ "м.Київ",
+    TRUE ~ oblast_name),
+    hromada_name = case_when(
+      settlement == "м. Київ" ~ "м.Київ", 
+      TRUE ~ hromada_name))
+
+ds1 %>% filter(is.na(hromada_name)) %>% view()
+# 527
+ds1 %>% filter(is.na(settlement), is.na(hromada_name)) %>% view()
+# 527
+
+#Part 2 getting more settlements
+ds1 <- ds1 %>%
   mutate(settlement = case_when(
-    is.na(settlement) ~ str_extract(name, "(м\\.|с\\.|смт\\.міста)\\s+(\\w+)"),
-    TRUE ~ settlement
-  )) %>%
-  select(name, address, formation_form, workers_total, square_total, 
-         state, hromada_name, settlement, council, oblast)
+    is.na(settlement) ~ str_extract(name, "(м\\.|с\\.|смт)\\s+([\\w’'-]+|м\\.[\\w’'-]+)"),
+    TRUE ~ settlement)) %>%
+  mutate(settlement = case_when(
+    is.na(settlement) ~ str_extract(name, "(?<=с\\.)\\w+"), 
+    TRUE ~ settlement)) %>%
+  select(code, name, address, formation_form, workers_total, square_total, 
+         state, hromada_name, settlement, council, oblast_name)
 
-ds1 %>% filter(is.na(hromada), is.na(settlement)) %>% view()
+ds1 %>% filter(is.na(settlement), is.na(hromada_name)) %>% view()
+#313
 
+# Part 3 - Finding hromada by council 
 
-# from councils and settlements
-joining_hromada <- ds1 %>% filter(is.na(hromada)) %>%
-  mutate(hromada = case_when(
-    council %in% ds_admin$full_name ~ ds_admin$hromada_name, # not working
-    settlement %in% ds_admin$settlement_name ~ ds_admin$hromada_name, 
-    TRUE ~ "я не знаю")
-  )
+# 3.1 - getting council parts from main dataset
+ds_council <- ds_admin %>%
+  mutate(council = str_extract(full_name, "([а-яА-ЯіІїЇєЄґҐ’'-]+)\\s+(сільради|сільської|міської|селищної)")
+         ,council = str_replace(council, "сільради", "сільської")) %>%
+  mutate(council = gsub("'", "’", council)) %>% 
+  select(council, oblast_name, hromada_code, hromada_name)#
 
+# 3.2 - getting rid of those council+oblast that duplicates
+ommit_hromada <- ds_council %>% 
+  distinct(council, oblast_name, hromada_code) %>% 
+  group_by(council, oblast_name) %>% count() %>%
+  filter(n > 1)
+ds_council <- ds_council %>% anti_join(ommit_hromada, by = c("council", "oblast_name"))
+         
+# 3.3 - finally joining all of those that corresponds
+joining_hromada <- ds1 %>% filter(is.na(hromada_name)) %>%
+  merge(ds_council, by = c("council", "oblast_name")) %>%
+  select(code, name, address, formation_form, workers_total, square_total, 
+         state, hromada_name.y, settlement, council, oblast_name, code) %>%
+  rename(hromada_name = hromada_name.y) %>%
+  distinct(code, .keep_all = TRUE) %>%
 
+# i did smth wrong cause it shouldn't be with distinct but i'm too tired to dig more
+
+ds2 <- ds1 %>% anti_join(joining_hromada, by = "code") %>%
+  bind_rows(joining_hromada)
+
+ds2 %>% filter(is.na(hromada_name)) %>% view()
+# 156 remained NA
+
+# Part 4 - joining by settlement admin ' j ’
+
+# 4.1 cleaning ds2
+joining_settlement <- ds2 %>% filter(is.na(hromada_name)) %>%
+  mutate(settlement_name = gsub("^(\\s*с\\.\\s*|\\s*м\\.\\s*|\\s*смт\\s*)", "", settlement)) %>%
+  select(code, name, address, formation_form, workers_total, square_total, 
+         state, hromada_name, settlement_name, council, oblast_name)
+
+# 4.2 - getting rid of those settlement+oblast that duplicates
+ommit_settlemet <- ds_admin %>% 
+  distinct(settlement_name, oblast_name, hromada_code) %>% 
+  group_by(settlement_name, oblast_name) %>% count() %>%
+  filter(n > 1)
+
+ds_settlement <- ds_admin %>% anti_join(ommit_settlemet, by = c("settlement_name", "oblast_name"))
+
+# 4.3 - joining
+joining_settlement <- joining_settlement %>% 
+  merge(ds_settlement, by = c("settlement_name", "oblast_name")) %>%
+  select(code, name, address, formation_form, workers_total, square_total, 
+       state, hromada_name.y, settlement_name, council, oblast_name, code) %>%
+  rename(hromada_name = hromada_name.y) %>%
+  distinct(code, .keep_all = TRUE) 
+
+ds3 <- ds2 %>% 
+  rename(settlement_name = settlement) %>% 
+  anti_join(joining_settlement, by = "code") %>%
+  bind_rows(joining_settlement)
+# finally agregating 
+
+ds3 %>% filter(is.na(hromada_name)) %>% view()
+
+# 90 are still with NA
+
+# n in hromada 
 #without 527 rows
-ready <- ds1 %>% filter(!is.na(hromada_name)) %>% 
+ready <- ds3 %>% filter(!is.na(hromada_name)) %>% 
   group_by(hromada_name) %>% 
   summarise(workers_total = sum(workers_total), 
             square_total = sum(square_total)) %>%
-  left_join(ds_hromada, by = "hromada_name")
+  left_join(ds_hromada, by = "hromada_name") # if no data put 0
 
-ready %>% filter(is.na(raion_name))%>% view()
+ready %>% filter(is.na(hromada_code)) %>% view() 
+# 33 з проблемами в апострофвх і тире знайти через ni
 
-# not informative, maybe replacing with our values?
-formation_form <- ds1 %>% group_by(hromada, formation_form) %>%
+
+# not informative, maybe replacing with our values - цнап чи відокремлене місце 
+formation_form <- ds3 %>% group_by(hromada_name, formation_form) %>%
   count() %>% pivot_wider(names_from = formation_form, values_from = n)
+# зробити структурний підрозділ + постійно діючий робочий орган + відалене робоче місце
