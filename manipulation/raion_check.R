b5d7f1f28eae5b590fc8a9195bfe29f80d1585f6
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2023-09-20 00:44:19 +0200

    Update raion_check.R

--- manipulation/raion_check.R
+++ manipulation/raion_check.R
@@ -1,6 +1,6 @@
 #new - old admin
 library(tidyverse)
-base <- readr::read_csv("./data-public/derived/ua-admin-map.csv") 
+base <- readr::read_csv("~/Documents/ua-de-center/data-public/derived/ua-admin-map.csv") 
 
 base_check <- base %>% select(settlement_name, settlement_code, hromada_name, hromada_code , raion_name, raion_code, 
                               raion_code_old, raion_name_old)
@@ -8,15 +8,15 @@ base_check <- base %>% select(settlement_name, settlement_code, hromada_name, hr
 base_check <- base_check %>% group_by(raion_code_old, hromada_code) %>% 
   mutate(former_raion = 0)
 
-base_check %>% filter(is.na(raion_code_old)) %>% view()
+base_check %>% filter(is.na(raion_name_old)) %>% view()
 
 same_city_regions <- list()
 
 # Iterate through unique old regions
-unique_old_regions <- unique(base_check$raion_code_old)
+unique_old_regions <- unique(base_check$raion_name_old)
 for (old_region in unique_old_regions) {
   # Extract cities associated with the current old region
-  cities_in_old_region <- base_check$settlement_code[base_check$raion_code_old == old_region]
+  cities_in_old_region <- base_check$settlement_code[base_check$raion_name_old == old_region]
   
   # Extract unique new regions for these cities
   unique_new_regions <- unique(base_check$hromada_code[base_check$settlement_code %in% cities_in_old_region])
@@ -25,13 +25,15 @@ for (old_region in unique_old_regions) {
   same_city_regions[[old_region]] <- unique_new_regions
 }
 
-# Print the results
+# fill in the results
 for (old_region in unique_old_regions) {
-  if (length(same_city_regions[[old_region]]) == 1 || 
-      (length(same_city_regions[[old_region]]) == 2 && NA %in% same_city_regions[old_region])) {  
-    base_check$former_raion[base_check$raion_code_old == old_region] <- 1
+  if (length(same_city_regions[[old_region]]) == 1) {  
+    base_check$former_raion[base_check$raion_name_old == old_region] <- 1
   }
-} # we need to proceed the NA test
+  if (is.na(old_region)) {
+    base_check$former_raion[base_check$raion_name_old == old_region] = 0
+  }
+}
 
 file_path <- "/ua-de-center/data-public/derived/former_raions.csv"
 
