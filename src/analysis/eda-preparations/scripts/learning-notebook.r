e4d1d9e9ce81f550dca06517810fec546a4ffc3b
ValHat <valgat29@gmail.com>
2025-04-25 18:21:13 +0300

    Create learning-notebook.r

---
+++ src/analysis/eda-preparations/scripts/learning-notebook.r
@@ -0,0 +1,4 @@
+## The script demonstrates and explains the use of main functions in the this repository
+## You can think of this as a serires of reproducible examples that summarize
+## what has been learned during the development of this project
+
