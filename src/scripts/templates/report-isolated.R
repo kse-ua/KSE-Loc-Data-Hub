7542f545590f4a061fe8f03b3b831fd853443742
ValHat <valgat29@gmail.com>
2025-04-25 18:16:24 +0300

    Create report-isolated.R

---
+++ src/scripts/templates/report-isolated.R
@@ -0,0 +1,66 @@
+rm(list = ls(all.names = TRUE)) # Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+cat("\014") # Clear the console
+# verify root location
+cat("Working directory: ", getwd()) # Must be set to Project Directory
+# Project Directory should be the root by default unless overwritten
+
+# ---- load-packages -----------------------------------------------------------
+# Choose to be greedy: load only what's needed
+# Three ways, from least (1) to most(3) greedy:
+# -- 1.Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(ggplot2)   # graphs
+library(forcats)   # factors
+library(stringr)   # strings, but consider `stringi` as more general
+library(lubridate) # dates
+library(labelled)  # labels
+# -- 2.Import only certain functions of a package into the search path.
+import::from("magrittr", "%>%")
+# -- 3. Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("readr"    )# data import/export
+requireNamespace("readxl"   )# data import/export
+requireNamespace("tidyr"    )# tidy data
+requireNamespace("janitor"  )# tidy data
+requireNamespace("testit"   )# for asserting conditions meet expected patterns.
+requireNamespace("scales"   )# formatting
+
+# ---- load-sources ------------------------------------------------------------
+base::source("./scripts/common-functions.R") # project-level
+
+# ---- declare-globals ---------------------------------------------------------
+# printed figures will go here when `quick_save("name",w=8,h=6)` is used:
+prints_folder <- paste0("./analysis/.../prints/")
+if (!fs::dir_exists(prints_folder)) { fs::dir_create(prints_folder) }
+
+path_data_input <- "./data-private/derived/..."
+# ---- declare-functions -------------------------------------------------------
+
+# ---- load-data ---------------------------------------------------------------
+ds0 <- readr::read_rds(path_data_input)
+
+# ---- inspect-data ------------------------------------------------------------
+
+
+# ---- tweak-data --------------------------------------------------------------
+
+
+# ---- table-1 -----------------------------------------------------------------
+
+
+# ---- graph-1 -----------------------------------------------------------------
+
+
+# ---- graph-2 -----------------------------------------------------------------
+
+# ---- save-to-disk ------------------------------------------------------------
+
+# ---- publish ------------------------------------------------------------
+path <- "./analysis/.../report-isolated.Rmd" # connect with Rmd for publishing
+rmarkdown::render(
+  input = path ,
+  output_format=c(
+    "html_document"
+    # "word_document"
+    # "pdf_document"
+  ),
+  clean=TRUE
+)
