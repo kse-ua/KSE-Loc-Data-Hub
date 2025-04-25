fe963a73a76881db33a4a8492bc782dc643b0c53
ValHat <valgat29@gmail.com>
2025-04-25 18:19:35 +0300

    Create workshop.R

---
+++ src/analysis/workshop/workshop.R
@@ -0,0 +1,12 @@
+## importing data from Github
+
+# data on competence
+path <- "https://raw.githubusercontent.com/kse-ua/ua-de-center/main/data-public/derived/community-competence-hromada.csv"
+data <- read.csv(path)
+
+# geographical data
+
+
+
+
+## 
